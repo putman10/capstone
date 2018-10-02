f69787f249c01ddf11d04673ca69635d66653b26
Student <example@example.com>
2018-10-02 14:14:59 -0700

    add delete function and route back to inbox for detail comment page

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -4,6 +4,9 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { markAsRead } from './../actions';
 import { withRouter } from 'react-router-dom';
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
+import { deleteComment } from './../actions';
 
 
 class Comment extends React.Component {
@@ -11,15 +14,24 @@ class Comment extends React.Component {
 		super(props)
 	}
 
+  deleteComment(selectedComment) {
+    const { dispatch, history } = this.props;
+    dispatch(deleteComment(selectedComment))
+    history.push('/admin');
+  }
+
 	render() {
     let selectedComment = this.props.comments[this.props.match.params.commentId];
 
     if(selectedComment) {
       return (
         <div className="container comment">
-          <p>{selectedComment.name}</p>
-          <p>{selectedComment.email}</p>
-          <p>{selectedComment.feedback}</p>
+          <p><Link to="/admin">Back to Inbox</Link></p>
+          <br />
+          <p><b>{selectedComment.name}</b> {selectedComment.email}</p>
+          <p><em><Moment>{selectedComment.timeSent}</Moment></em></p>
+          <p className="feedbackArea">{selectedComment.feedback}</p>
+          <button className="commentDetailButton" onClick={() =>this.deleteComment(selectedComment)}>DELETE</button>
         </div>
       )
     } else {
